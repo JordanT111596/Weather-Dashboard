68fa860e521942dce4b4afed81b698d638e904b6
Jordan Triplett <JordanT111596@gmail.com>
2020-08-13 18:40:22 -0400

    entered searches now appear as list item

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 //event listener that logs the user input using either the button or the enter key
 var apiKey = "863a6db584ee09579b62dfe7cf104c44";
+var cityArr = [];
 
 $("#search-button").on("click", function (event) {
     //stops page from refreshing upon hitting enter key
@@ -7,4 +8,18 @@ $("#search-button").on("click", function (event) {
     //stores user input into variable
     var userText = $("#search-input").val();
     console.log(userText);
+    renderCityList(userText);
+    //cityArr.push(userText);
 });
+
+function renderCityList (name) {
+    cityArr.push(name);
+    for (var i = 0; i < cityArr.length; i++) {
+        var newLi = $("<li>");
+        newLi.text(cityArr[i]);
+        newLi.addClass("list-group-item");
+        newLi.addClass("city");
+        newLi.attr("id", "city" + i);
+        $("#cityList").append(newLi);
+    }
+}
