bd32b31e7483003a408b8052a2e1c9b7d4034923
Jordan Triplett <JordanT111596@gmail.com>
2020-08-13 16:05:02 -0400

    Added apikey variable to js & added ul for cityList

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 //event listener that logs the user input using either the button or the enter key
+var apiKey = "863a6db584ee09579b62dfe7cf104c44";
+
 $("#search-button").on("click", function (event) {
     //stops page from refreshing upon hitting enter key
     event.preventDefault();
